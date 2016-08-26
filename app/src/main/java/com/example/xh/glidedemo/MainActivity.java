f94b848ad2b78d70611a750ae32a06fec5a0aad9
xiehui <xiehui0902@gmail.com>
2016-08-26 08:40:54 +0800

    Acquaintance Glide
    Acquaintance Glide!

--- app/src/main/java/com/example/xh/glidedemo/MainActivity.java
+++ app/src/main/java/com/example/xh/glidedemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.xh.glidedemo;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Context context;
+    private String[] urls = {"http://img2.3lian.com/2014/f6/173/d/51.jpg", "http://img2.3lian.com/2014/f6/173/d/52.jpg",
+            "http://img2.3lian.com/2014/f6/173/d/53.jpg", "http://img2.3lian.com/2014/f6/173/d/54.jpg",
+            "http://img2.3lian.com/2014/f6/173/d/55.jpg", "http://img2.3lian.com/2014/f6/173/d/56.jpg",
+            "http://img16.3lian.com/gif2016/q21/43/81.jpg", "http://img16.3lian.com/gif2016/q21/43/84.jpg",
+            "http://img16.3lian.com/gif2016/q21/43/87.jpg"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +30,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        this.context = this;
+        imageView = (ImageView) findViewById(R.id.imageView);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        glideLoadImage();
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void glideLoadImage() {
+        int  resourceId=R.mipmap.image;
+        Glide.with(context).load(resourceId).into(imageView);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
